f9d0d89c25537e910dbc4ce008f8074ade7724fa
Ajinkya Bangar <ajinkyabangar212@gmail.com>
2022-07-20 18:06:52 +0530

    Add project files.

---
+++ CURD Using Dapper Framework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CURD_Using_Dapper_Framework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
